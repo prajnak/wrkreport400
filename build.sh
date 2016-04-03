95f27f4ff1dd4a7014a81b7bf261142faa685574
Prajna Kandarpa <prajna.kandarpa@uwaterloo.ca>
2016-04-02 23:35:24 -0400

    resubmit

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ pdflatex --shell-escape uw-ethesis
 pdflatex --shell-escape uw-ethesis
 
 # pkill Preview
-open -a Preview uw-ethesis.pdf; 
+cp uw-ethesis.pdf wrkreport400_resubmit.pdf
+open -a Preview wrkreport400_resubmit.pdf; 
 # /usr/bin/osascript -e 'tell application "Preview"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell"
